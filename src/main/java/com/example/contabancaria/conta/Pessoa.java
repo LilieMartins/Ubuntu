59e5436c07948c1a6b4fcfd8187f920b127c694e
Liliane <liliane.angelomartins@gmail.com>
2020-07-20 21:11:53 -0300

    Primeira subida na master

---
+++ src/main/java/com/example/contabancaria/conta/Pessoa.java
@@ -0,0 +1,37 @@
+package com.example.contabancaria.conta;
+
+public class Pessoa {
+    private String nome;
+    private Integer cpf;
+    private String dataNascimento;
+
+    public Pessoa(String nome, Integer cpf, String dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
